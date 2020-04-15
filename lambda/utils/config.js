84d21d3d6f7e3a734312ee419b8a08b7e464d330
4480135 <4480135@RTS-365.dk>
2020-04-15 13:02:19 +0200

    Fix attempt.

--- lambda/utils/config.js
+++ lambda/utils/config.js
@@ -2,10 +2,12 @@
 // Circumvent problem with Netlify CLI.
 // https://github.com/netlify/netlify-dev-plugin/issues/147
 
+console.log(process.env.NODE_ENV);
+
 exports.BASE_URL = process.env.NODE_ENV === 'development'
   ? 'http://localhost:8888'
   : process.env.BASE_URL;
-  
+
 exports.COOKIE_SECURE = process.env.NODE_ENV !== 'development';
 
 exports.ENDPOINT = process.env.NODE_ENV === 'development'
